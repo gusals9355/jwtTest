c585824b32e78cd072bd13cb61db47e0964cec12
gusals9355 <gusals9355@naver.com>
2021-07-12 21:50:44 +0900

    0712

--- src/main/java/com/example/jwttest/jwt/JwtProperties.java
+++ src/main/java/com/example/jwttest/jwt/JwtProperties.java
@@ -2,7 +2,7 @@ package com.example.jwttest.jwt;
 
 public interface JwtProperties {
     String SECRET = "mndc"; //우리 서버만 알고 있는 비밀값
-    int EXPIRATTION_TIME = 3600000; //(1000 * 60 * 60)1시간
+    int EXPIRATTION_TIME = 8640000; //(1000 * 60 * 60 * 24) 24시간
     String TOKEN_PREFIX = "Bearer ";
     String HEADER_STRING = "Authorization";
 }
